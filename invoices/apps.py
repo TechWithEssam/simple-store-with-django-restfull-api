c1a65e98c83c6eb38e77d11bc4e9a99a3da71308
TechWithEssam <techwithessam@gmail.com>
2023-02-11 00:11:10 +0200

    delete some errors

--- invoices/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class InvoicesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'invoices'
-    def ready(self) :
-        import invoices.signals
